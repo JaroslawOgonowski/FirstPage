9303a850730006a40687e6d4745c8c77af632e88
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2022-10-13 21:18:19 +0200

    add table responsive, paragraph margin, txt edit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 {
     const welcome = () => {
-    console.log("Hello stranger");
-    init();
-}
-
-const button = document.querySelector(".js-button")
-const main = document.querySelector(".js-main")
-
-const init = () => {
-button.addEventListener("click", () => {
-    switch (button.innerText) {
-
-    case "Nox":
-    return button.innerText = "Lumos", main.classList.add("main--nox");
-    case "Lumos":
-    return button.innerText = "Nox", main.classList.remove("main--nox");
+        console.log("Hello stranger");
+        init();
     }
-});
-}
 
-welcome();
+    const button = document.querySelector(".js-button")
+    const main = document.querySelector(".js-main")
+
+    const init = () => {
+        button.addEventListener("click", () => {
+            switch (button.innerText) {
+
+                case "Nox":
+                    return button.innerText = "Lumos", main.classList.add("main--nox");
+                case "Lumos":
+                    return button.innerText = "Nox", main.classList.remove("main--nox");
+            }
+        });
+    }
+
+    welcome();
 }
